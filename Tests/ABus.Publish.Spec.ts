dec663adf4e6dda6e9db83e36717cae967447011
Garry McGlennon <garrm@microsoft.com>
2016-11-16 23:23:29 -0800

    inital base with tests

---
+++ Tests/ABus.Publish.Spec.ts
@@ -0,0 +1,154 @@
+import {
+    MessagePipeline,
+    MessageHandlerContext,
+    IMessage,
+    IMessageSubscription,
+    IMessageHandlerContext,
+    MessageHandlerOptions,
+    ThreadingOptions,
+    Utils
+} from '../ABus';
+
+import * as testData from './ABus.Sample.Messages'
+
+describe("publishing a message outside of a handler", () => {
+    var pipeline = new MessagePipeline();
+    var returnedMessage: testData.CustomerData;
+    var currentHandlerContext: IMessageHandlerContext;
+    var counter = 0;
+
+    pipeline.subscribe({
+        messageType: testData.TestMessage.TYPE,
+        handler: (message: testData.CustomerData, context: MessageHandlerContext) => {
+            returnedMessage = message;
+            currentHandlerContext = context;
+        }
+    }, MessageHandlerOptions.Synchronous());
+
+    pipeline.subscribe({
+        messageType: testData.TestMessage2.TYPE,
+        handler: (message: testData.CustomerData, context: MessageHandlerContext):Promise<any> => {
+            returnedMessage = message;
+            currentHandlerContext = context;
+            return Utils.sleep(5).then(()=>{
+                // Now set the counter
+                counter = 10;
+            });
+        }
+    });
+
+    it("should send message to all registered subscribers", () => {
+        pipeline.publish(new testData.TestMessage("Johhny Smith"));
+        expect(returnedMessage.name).toBe("Johhny Smith");
+    });
+
+    it("should add a messageHandlerContext to the handler recieving message being sent", () => {
+        pipeline.publish(new testData.TestMessage("Johhny Smith"));
+        expect(currentHandlerContext).toBeDefined();
+    });
+
+    it("should add messageType to messageHandlerContext", () => {
+        expect(currentHandlerContext.messageType).toBe(testData.TestMessage.TYPE);
+    });
+
+    it("should add messageId to messageHandlerContext", () => {
+        expect(currentHandlerContext.messageId).toBeDefined();
+    });
+
+    it("should set the conversationId on messageHandlerContext", () => {
+        expect(currentHandlerContext.conversationId).toBeDefined();
+    });
+
+    it("should set the correlationId on messageHandlerContext to undefined", () => {
+        // Messages outside of a handler are not part of an existing conversation
+        expect(currentHandlerContext.correlationId).toBeUndefined();
+    });
+
+    it("should not throw an exception if subscriber throws an exception", () => {
+        // subscribe to a msg that will then fail during its processing.
+        pipeline.subscribe({
+            messageType: testData.TestMessage.TYPE,
+            handler: (message: testData.CustomerData, context: MessageHandlerContext) => {
+                throw new TypeError("Boom!!");
+            }
+        }, MessageHandlerOptions.Synchronous());
+
+        pipeline.publish({ type: testData.TestMessage.TYPE, message: new testData.TestMessage("") });
+        return Utils.sleep(10)
+            .then(() => { expect(true) })
+            .catch(() => {
+                throw new TypeError("no error should have been caught");
+            })
+    });
+
+    it("should be fully async and return before subscribers have processed the message", () => {
+        counter = 0;
+        debugger;
+
+        pipeline.publish(new testData.TestMessage2("test"));
+        expect(counter).toBe(0);
+        return Utils.sleep(30).then(() => {
+            expect(counter).toBe(10);
+        });
+    });
+});
+
+
+describe("publishing a message inside of a handler", () => {
+    var pipeline = new MessagePipeline();
+    var firstMessage: testData.CustomerData;
+    var firstHandlerContext: IMessageHandlerContext;
+
+    var secondMessage: testData.CustomerData;
+    var secondHandlerContext: IMessageHandlerContext;
+
+    pipeline.subscribe({
+        messageType: testData.TestMessage.TYPE,
+        handler: (message: testData.CustomerData, context: MessageHandlerContext) => {
+            firstMessage = message;
+            firstHandlerContext = context;
+            context.publish(new testData.TestMessage2("second message"));
+        }
+    });
+
+    pipeline.subscribe({
+        messageType: testData.TestMessage2.TYPE,
+        handler: (message: testData.CustomerData, context: MessageHandlerContext) => {
+            secondMessage = message;
+            secondHandlerContext = context;
+        }
+    });
+
+    pipeline.publish(new testData.TestMessage("Johhny Smith"));
+
+    it("should send message to all registered subscribers", () => {
+        return Utils.sleep(10)
+            .then(() => {
+                expect(secondHandlerContext.messageType).toBe("test.message2");
+                expect(secondMessage.name).toBe("second message");
+            });
+    });
+
+    it("should add a messageHandlerContext to the handler recieving message being sent", () => {
+        expect(secondHandlerContext.messageType).toBe("test.message2");
+        expect(secondHandlerContext).toBeDefined();
+    });
+
+    it("should add messageId to messageHandlerContext which differs from original message", () => {
+        expect(secondHandlerContext.messageType).toBe("test.message2");
+        expect(secondHandlerContext.messageId).toBeDefined();
+        expect(secondHandlerContext.messageId).not.toBe(firstHandlerContext.messageId);
+    });
+
+    it("should set the conversationId on messageHandlerContext to the same as the original message", () => {
+        expect(secondHandlerContext.messageType).toBe("test.message2");
+        expect(secondHandlerContext.conversationId).toBeDefined();
+        expect(secondHandlerContext.conversationId).toBe(firstHandlerContext.conversationId);
+    });
+
+    it("should set the correlationId on messageHandlerContext to the messageId of the original message", () => {
+        expect(secondHandlerContext.messageType).toBe("test.message2");
+        expect(secondHandlerContext.correlationId).toBeDefined();
+        expect(secondHandlerContext.correlationId).toBe(firstHandlerContext.messageId);
+    });
+});
---
+++ Tests/hashtable.spec.ts
@@ -0,0 +1,120 @@
+import Hashtable from '../hashtable';
+
+describe("Creating instance", () => {
+    var ht = new Hashtable<string>();
+    ht.add("test.key", "test.value");
+
+    it("should support generic types", () => {
+        let ht = new Hashtable<number>();
+        ht.add("key", 2334)
+        expect(ht.count).toBe(1);
+    });
+})
+
+
+describe("Adding an item to a hashtable", () => {
+    var ht = new Hashtable<string>();
+    ht.add("test.key", "test.value");
+
+    it("should increase the count by 1", () => {
+        expect(ht.count).toBe(1);
+    });
+
+    it("should allow the retrieval of the value by key", () => {
+        expect(ht.item("test.key")).toBe("test.value");
+    });
+
+    it("should contain the key", () => {
+        expect(ht.contains("test.key")).toBe(true);
+    });
+
+    it("should throw duplicate exception if key already exists", () => {
+        // need to wrap errors in its own function
+        var key = "test.key";
+        var duplicateKey = () => {
+            ht.add(key, "");
+        }
+        expect(duplicateKey).toThrowError(`The key ${key} already exists.`);
+    });
+
+    it("should include the new item in keys collection", () => {
+        expect(ht.keys()[0]).toBe("test.key");
+    });
+
+    it("should be returned in for .. in", () => {
+        let foundItems = [];
+        for (let item of ht.items()) {
+            foundItems.push(item);
+        }
+        expect(foundItems.length).toBe(1);
+        expect(foundItems[0]).toBe("test.value");
+    });
+    
+});
+
+describe("Removing an item from a hashtable", () => {
+    var ht = new Hashtable<string>();
+
+    // Arrange
+    ht.add("test.key1", "test.value");
+    ht.add("test.key2", "test.value");
+    ht.add("test.key", "test.value");
+
+    // Act
+    ht.remove("test.key");
+
+    // Assert
+    it("should decrease the count by 1", () => {
+        expect(ht.count).toBe(2);
+    });
+
+    it("should disallow the retrieval of the value by key", () => {
+        expect(ht.item("test.key")).toBe(undefined);
+    });
+
+    it("should not contain the key", () => {
+        expect(ht.contains("test.key")).toBe(false);
+    });
+
+    it("should not contain item in keys collection", () => {
+        expect(ht.keys().length).toBe(2);
+    });
+})
+
+describe("Clearing the hashtable", () => {
+    var ht = new Hashtable<string>();
+
+    // Arrange
+    ht.add("test.key1", "test.value");
+    ht.add("test.key2", "test.value");
+    ht.add("test.key", "test.value");
+
+    // Act
+    ht.clear();
+
+    // Assert
+    it("should have a count of 0", () => {
+        expect(ht.count).toBe(0);
+    });
+
+    it("should have no values for the keys collection", () => {
+        expect(ht.keys().length).toBe(0);
+    });
+})
+
+describe("Updaing an item to a hashtable", () => {
+    var ht = new Hashtable<string>();
+
+    it("should add the item if not found", () => {
+        ht.clear();
+        ht.update("test.key", "test.value");
+        expect(ht.item("test.key")).toBe("test.value");
+    });
+    
+    it("should update the item if already exist", () => {
+        ht.clear();
+        ht.add("test.key", "test.value2");
+        ht.update("test.key", "test.value");
+        expect(ht.item("test.key")).toBe("test.value");
+    });
+});
